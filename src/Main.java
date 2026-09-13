e53c409bdd140a8f2be9f9dd1d1e6a3da63b36e1
VlachynskyPetr <peta777vlach@gmail.com>
2026-09-13 16:08:33 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    System.out.println("O prázninách jsem se měl skvěle." + "\n" +
+            "Pracoval jsem na zahradě a pomáhal jsem rodičům." + "\n" +
+            "Každý den jsem snídal chleba s mlékem." + "\n" +
+            "Několikrát jsem navštívil babičku a dědečka." + "\n" +
+            "Tento rok jsem nebyl na dovolené a vůbec mi to nevadí.");
+}
